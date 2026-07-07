84087cfb8e634d4875aa69eba29b762a02a289a9
Micah Vella <mi938561@ucf.edu>
2026-07-06 21:59:04 -0400

    Add files via upload

---
+++ songs.js
@@ -0,0 +1,62 @@
+export const SONGS_DB = [
+  {
+    id: 1,
+    title: "Blinding Lights",
+    artist: "The Weeknd",
+    album: "After Hours",
+    type: "song",
+    listened: false,
+    rating: null,
+    cover: "albumcovers/The_Weeknd_-_Blinding_Lights.png"
+  },
+  {
+    id: 2,
+    title: "Future Nostalgia",
+    artist: "Dua Lipa",
+    album: "Future Nostalgia",
+    type: "album",
+    listened: false,
+    rating: null,
+    cover: "albumcovers/Dua_Lipa_-_Future_Nostalgia_(Official_Album_Cover).png"
+  },
+  {
+    id: 3,
+    title: "Shape of You",
+    artist: "Ed Sheeran",
+    album: "Divide",
+    type: "song",
+    listened: false,
+    rating: null,
+    cover: "albumcovers/Shape_Of_You_(Official_Single_Cover)_by_Ed_Sheeran.png"
+  },
+  {
+    id: 4,
+    title: "Say It Ain't So",
+    artist: "Weezer",
+    album: "Weezer (Blue)",
+    type: "song",
+    listened: false,
+    rating: null,
+    cover: "albumcovers/Weezer_-_Blue_Album.png"
+  },
+  {
+    id: 5,
+    title: "Deltarune Chapter 5 OST",
+    artist: "Toby Fox",
+    album: "Deltarune Chapter 5 OST",
+    type: "album",
+    listened: false,
+    rating: null,
+    cover: "albumcovers/deltarune_chapter_5_ost.jpg"
+  },
+  {
+    id: 6,
+    title: "Piano Man",
+    artist: "Billy Joel",
+    album: "Piano Man",
+    type: "song",
+    listened: false,
+    rating: null,
+    cover: "albumcovers/Billy_Joel_-_Piano_Man.jpg"
+  }
+];
